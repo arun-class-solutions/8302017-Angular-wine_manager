68e7b8a977bbb04725766df76f0c17a8af1420f5
Arun Sood <arsood@gmail.com>
2017-08-30 14:32:16 -0700

    POST to add wine

--- app.js
+++ app.js
@@ -9,4 +9,21 @@ app.controller("wineListCtrl", function($scope, $http) {
     }, function(err) {
         console.log(err);
     });
+
+    $scope.submitWine = function(event) {
+        event.preventDefault();
+
+        $http
+        .post("http://myapi-profstream.herokuapp.com/api/707381/wines", $scope.wine)
+        .then(function(newWine) {
+            // Step 1: Take newly-created wine object and add it to the UI
+            // Step 2: Close the modal window
+            // Hint: To close the modal -> $("#add-wine-modal").modal("hide");
+            $scope.wines.push(newWine.data);
+
+            $("#add-wine-modal").modal("hide");
+        }, function(err) {
+            console.log(err);
+        });
+    }
 });
